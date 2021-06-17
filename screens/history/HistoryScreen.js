eea1e1a5e5e9bd7487fa906c101dcf73b7996938
Thien Quy Tran <tranthienquy98@gmail.com>
2021-06-17 19:29:41 +0700

    quytt

--- screens/history/HistoryScreen.js
+++ screens/history/HistoryScreen.js
@@ -8,17 +8,22 @@ import {Text as SvgText} from 'react-native-svg';
 import {Image as SvgImage} from 'react-native-svg';
 
 const HistoryScreen = () => {
+
+     const onPressDetailHistory = () => {
+    navigation.navigate("DetailHistory")
+
+  }
     return (
     <ScrollView style={styles.history}>
         <View style={styles.history_rectangle15}></View>
         <Svg style={styles.history_path6} preserveAspectRatio="none" viewBox="0 -0.000003814697265625 393 743.6717529296875" fill="rgba(249, 249, 249, 1)"><SvgPath d="M 60 0 L 333 0 C 366.1370849609375 0 393 25.64466285705566 393 57.27895736694336 L 393 743.6717529296875 L 0 743.6717529296875 L 0 57.27895736694336 C 0 25.64466285705566 26.8629150390625 0 60 0 Z"  /></Svg>
-        <View style={styles.history_group59}>
+        <TouchableOpacity onPress={onPressDetailHistory} style={styles.history_group59}>
             <View style={styles.history_group59_group58}>
                 <Svg style={styles.history_group59_group58_path7} preserveAspectRatio="none" viewBox="0 0 118 118" fill="rgba(249, 174, 81, 1)"><SvgPath d="M 23 0 L 95 0 C 107.7025527954102 0 118 10.29745006561279 118 23 L 118 95 C 118 107.7025527954102 107.7025527954102 118 95 118 L 23 118 C 10.29745006561279 118 0 107.7025527954102 0 95 L 0 23 C 0 10.29745006561279 10.29745006561279 0 23 0 Z"  /></Svg>
                 <Text style={styles.history_group59_group58_ban1}>Bàn 1</Text>
                 <Text style={styles.history_group59_group58_x2Ngi}>2 người</Text>
             </View>
-        </View>
+        </TouchableOpacity>
         <Text style={styles.history_lchS}>Lịch sử</Text>
         <Text style={styles.history_sBan6}>Số bàn: 6</Text>
         <Svg style={styles.history_ellipse1} preserveAspectRatio="none" viewBox="0 0 44 44" fill="rgba(255, 255, 255, 0)"><Defs><Pattern id="img-ellipse1" patternContentUnits="userSpaceOnUse" width="100%" height="100%"><SvgImage xlinkHref={require('./assets/ellipse1.png')} x="0" y="0" width="44.00px" height="44.00px" /></Pattern></Defs><SvgPath d="M 22 0 C 34.15026473999023 0 44 9.849736213684082 44 22 C 44 34.15026473999023 34.15026473999023 44 22 44 C 9.849736213684082 44 0 34.15026473999023 0 22 C 0 9.849736213684082 9.849736213684082 0 22 0 Z" fill="url(#img-ellipse1)" /></Svg>
