d0dede0f6ac758a12e7526809c97edf7ad071d08
Nguyễn Hiếu Đẹp Trai <hieuntse63066@fpt.edu.vn>
2021-06-17 17:26:15 +0700

    hieu changed

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from "./screens/welcome/WelcomeScreen";
 import RegistryScreen from './screens/registry/RegisterScreen';
 import LoginScreen from './screens/login/LoginScreen';
+import HomeScreen from './screens/home/HomeScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,6 +17,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Registry" component={RegistryScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
