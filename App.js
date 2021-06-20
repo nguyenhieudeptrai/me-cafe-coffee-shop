37480fc97dd425f7a9e24f19a2bb4e6186d79156
Thien Quy Tran <tranthienquy98@gmail.com>
2021-06-20 16:52:24 +0700

    quytt

--- App.js
+++ App.js
@@ -18,22 +18,15 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-<<<<<<< HEAD
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="DetaiHis" component={DetailHistory} options={{ headerShown: false }} />
-=======
-      <Stack.Navigator initialRouteName="AddVoucher">
         <Stack.Screen name="DetailHistory" component={DetailHistory} options={{ headerShown: false }} />
->>>>>>> 0444c62e91835a0f1e53f6ce814f6d7112bbfbce
         <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Registry" component={RegistryScreen} options={{ headerShown: false }} />
-<<<<<<< HEAD
-=======
         <Stack.Screen name="Voucher" component={VoucherScreen} options={{ headerShown: false }} />
         <Stack.Screen name="AddVoucher" component={addvoucherScreen} options={{ headerShown: false }} />
->>>>>>> 0444c62e91835a0f1e53f6ce814f6d7112bbfbce
       </Stack.Navigator>
     </NavigationContainer>
   );
