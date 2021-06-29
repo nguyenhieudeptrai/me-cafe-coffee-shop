651ee02ad9256aa4214fd1203c8f4451a602f259
Nguyễn Hiếu Đẹp Trai <hieuntse63066@fpt.edu.vn>
2021-06-29 22:34:20 +0700

    edit

--- screens/home/tab/VoucherTab.js
+++ screens/home/tab/VoucherTab.js
@@ -11,7 +11,6 @@ const VoucherTab = ({ navigation }) => {
   const onPressAddVoucher = () => {
     navigation.navigate("AddVoucher");
   }
-<<<<<<< HEAD
 
   return (
     <ScrollView style={styles.vouchermain}>
@@ -20,17 +19,6 @@ const VoucherTab = ({ navigation }) => {
       </View>
       <View style={styles.vouchermain_list}>
         <View style={styles.vouchermain_list_item}>
-=======
-  return (
-    <ScrollView style={styles.vouchermain}>
-      <View style={styles.vouchermain_group61}>
-        <View style={styles.vouchermain_group61_rectangle15}></View>
-        <Text style={styles.vouchermain_group61_voucher}>VOUCHER</Text>
-      </View>
-      <View style={styles.vouchermain_list}>
-        <View style={styles.vouchermain_list_item}>
-          <View style={styles.vouchermain_list_item_rectangle32}></View>
->>>>>>> ffa0a10d02ea68e9e901d00b197e367b4e49fc90
           <View style={styles.vouchermain_list_item_right}>
             <Text style={styles.vouchermain_list_item_right_title77d99b77}>Ưu đãi 20% toàn menu</Text>
             <View style={styles.vouchermain_list_item_right_status}>
@@ -51,21 +39,12 @@ const VoucherTab = ({ navigation }) => {
                 <View style={styles.vouchermain_list_item_left_type_bgfc50470f_rectangle1482}></View>
                 <View style={styles.vouchermain_list_item_left_type_bgfc50470f_rectangle1483}></View>
               </View>
-<<<<<<< HEAD
               <Text style={styles.vouchermain_list_item_left_type_name}>Ưu đãi</Text>
-=======
-              <Text style={styles.vouchermain_list_item_left_type_name}>Delivery</Text>
->>>>>>> ffa0a10d02ea68e9e901d00b197e367b4e49fc90
             </View>
           </View>
         </View>
         <TouchableOpacity onPress={onPressAddVoucher} style={styles.vouchermain_list_btnAdd}>
-          <View style={styles.vouchermain_list_btnAdd_bg}></View>
-<<<<<<< HEAD
           <Text style={styles.vouchermain_list_btnAdd_title}>Thêm khuyễn mãi</Text>
-=======
-          <Text style={styles.vouchermain_list_btnAdd_title}>Thêm Vourcher</Text>
->>>>>>> ffa0a10d02ea68e9e901d00b197e367b4e49fc90
           <Svg style={styles.vouchermain_list_btnAdd_addIcon} preserveAspectRatio="none" viewBox="0.5625 0.5625 34.875 34.875" fill="rgba(216, 174, 66, 1)"><SvgPath d="M 18 0.5625 C 8.3671875 0.5625 0.5625 8.3671875 0.5625 18 C 0.5625 27.6328125 8.3671875 35.4375 18 35.4375 C 27.6328125 35.4375 35.4375 27.6328125 35.4375 18 C 35.4375 8.3671875 27.6328125 0.5625 18 0.5625 Z M 28.125 19.96875 C 28.125 20.43281173706055 27.74531173706055 20.8125 27.28125 20.8125 L 20.8125 20.8125 L 20.8125 27.28125 C 20.8125 27.74531173706055 20.43281173706055 28.125 19.96875 28.125 L 16.03125 28.125 C 15.56718730926514 28.125 15.1875 27.74531173706055 15.1875 27.28125 L 15.1875 20.8125 L 8.71875 20.8125 C 8.254687309265137 20.8125 7.875 20.43281173706055 7.875 19.96875 L 7.875 16.03125 C 7.875 15.56718730926514 8.254687309265137 15.1875 8.71875 15.1875 L 15.1875 15.1875 L 15.1875 8.71875 C 15.1875 8.254687309265137 15.56718730926514 7.875 16.03125 7.875 L 19.96875 7.875 C 20.43281173706055 7.875 20.8125 8.254687309265137 20.8125 8.71875 L 20.8125 15.1875 L 27.28125 15.1875 C 27.74531173706055 15.1875 28.125 15.56718730926514 28.125 16.03125 L 28.125 19.96875 Z" /></Svg>
         </TouchableOpacity>
       </View>
@@ -88,22 +67,9 @@ VoucherTab.defaultProps = {
 const styles = StyleSheet.create({
   "vouchermain": {
 
-<<<<<<< HEAD
     // "backgroundColor": "rgba(249, 249, 249, 1)",
     backgroundColor: "rgba(220, 220, 220, 1)",
     flex: 1
-=======
-    "backgroundColor": "rgba(249, 249, 249, 1)",
-    flex: 1
-  },
-  "vouchermain_rectangle22": {
-    "opacity": 1,
-    "position": "absolute",
-    "backgroundColor": "rgba(219, 219, 219, 1)",
-    "width": 393,
-    "height": 851,
-
->>>>>>> ffa0a10d02ea68e9e901d00b197e367b4e49fc90
   },
   "vouchermain_header": {
     "backgroundColor": "rgba(241, 211, 126, 1)",
@@ -119,21 +85,16 @@ const styles = StyleSheet.create({
     },
     "shadowRadius": 6,
     "width": "100%",
-    marginBottom:10
+    marginBottom: 10
   },
   "vouchermain_header_title": {
     "color": "rgba(84, 71, 65, 1)",
     "fontSize": 20,
     "fontWeight": "700",
     "fontFamily": "Roboto",
-<<<<<<< HEAD
-    alignSelf:"center",
-    marginTop:40,
-    marginBottom:15
-=======
-    "textAlign": "left",
-    "top": 45,
-    "alignSelf": 'center'
+    alignSelf: "center",
+    marginTop: 40,
+    marginBottom: 15,
   },
   "vouchermain_list": {
     "opacity": 1,
@@ -143,26 +104,10 @@ const styles = StyleSheet.create({
     "height": 212,
     "alignSelf": 'center',
     "top": 105
->>>>>>> ffa0a10d02ea68e9e901d00b197e367b4e49fc90
   },
   "vouchermain_list_item": {
-    "opacity": 1,
-    "position": "absolute",
-<<<<<<< HEAD
     "backgroundColor": "#FFF",
     borderRadius: 7,
-=======
-    "backgroundColor": "transparent",
-    "width": "95%",
-    "height": 97,
-    "alignSelf": 'center'
-  },
-  "vouchermain_list_item_rectangle32": {
-    "opacity": 1,
-    "position": "absolute",
-    "backgroundColor": "rgba(255, 255, 255, 1)",
-    "borderRadius": 7,
->>>>>>> ffa0a10d02ea68e9e901d00b197e367b4e49fc90
     "shadowOpacity": 0.16,
     "shadowOffset": {
       "width": 0,
@@ -171,8 +116,7 @@ const styles = StyleSheet.create({
     "shadowRadius": 5,
     "width": "100%",
     "height": 97,
-    "left": 0,
-    "top": 0
+    marginBottom:10,
   },
   "vouchermain_list_item_right": {
     "opacity": 1,
@@ -361,20 +305,7 @@ const styles = StyleSheet.create({
     "top": 1
   },
   "vouchermain_list_btnAdd": {
-    "backgroundColor": "transparent",
-    "width": "100%",
-    "height": 97,
-    "left": 0,
-    "top": 115
-  },
-  "vouchermain_list_btnAdd_bg": {
-    "opacity": 1,
-    "position": "absolute",
-    "backgroundColor": "rgba(255, 255, 255, 1)",
-    "borderTopLeftRadius": 7,
-    "borderTopRightRadius": 7,
-    "borderBottomLeftRadius": 7,
-    "borderBottomRightRadius": 7,
+    "borderRadius": 7,
     "shadowColor": "rgb(0,  0,  0)",
     "shadowOpacity": 0.16,
     "shadowOffset": {
@@ -382,10 +313,10 @@ const styles = StyleSheet.create({
       "height": 3
     },
     "shadowRadius": 5,
-    "width": "95%",
+    "width": "100%",
     "height": 97,
     "alignSelf": 'center',
-    "top": 0
+    backgroundColor:"#FFF"
   },
   "vouchermain_list_btnAdd_title": {
     "opacity": 0.74,
